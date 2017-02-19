89bd4db576ae6bae0b577e3901b9d1866ab5667c
Andrey Tsvetkov <ts.andrey@gmail.com>
2017-02-19 23:03:29 +0300

    packages updated as at 19 Feb 2017

--- webpack.config.js
+++ webpack.config.js
@@ -4,9 +4,9 @@ var CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     context: __dirname,
-    entry: { 
-        bundle: './src/index'//, 
-        //test: './tests/test'
+    entry: {
+        bundle: './src/index' //, 
+            //test: './tests/test'
     },
     resolve: {
         extensions: ['.ts', '.tsx', '.js', '.jsx']
@@ -16,51 +16,49 @@ module.exports = {
         path: path.join(__dirname, 'www'),
         filename: '[name].js'
     },
-    externals: { 
-        react: 'React', 
-        'react-bootstrap': 'ReactBootstrap', 
+    externals: {
+        react: 'React',
+        'react-bootstrap': 'ReactBootstrap',
         'react-dom': 'ReactDOM'
-    }, 
+    },
     module: {
-        loaders: [
-            { 
-                test: /\.tsx?$/, 
-                loader: 'ts-loader', 
+        loaders: [{
+                test: /\.tsx?$/,
+                loader: 'ts-loader',
                 exclude: '/node_modules/'
-            }, 
+            },
             {
                 test: /\.css$/,
-                loader: 'style!css'
+                loader: 'style-loader!css-loader'
             },
             {
                 test: /\.less$/,
-                loader: 'style!css!less'
+                loader: 'style-loader!css-loader!less-loader'
             }
         ]
-    }, 
+    },
     plugins: [
         new webpack.optimize.UglifyJsPlugin({
-          compress: { warnings: false },
-          output: { comments: false },
-        }), 
+            compress: { warnings: false },
+            output: { comments: false },
+        }),
         //extractCss,
         //commonsChunk, 
-        new webpack.HotModuleReplacementPlugin(), 
-        new CopyWebpackPlugin([
-            { 
-                from: path.join(__dirname, 'src', 'index.html'), 
+        new webpack.HotModuleReplacementPlugin(),
+        new CopyWebpackPlugin([{
+                from: path.join(__dirname, 'src', 'index.html'),
                 to: path.join(__dirname, 'www', 'index.html')
-            }, 
-            { 
-                from: path.join(__dirname, 'node_modules', 'react', 'dist', 'react.min.js'), 
+            },
+            {
+                from: path.join(__dirname, 'node_modules', 'react', 'dist', 'react.min.js'),
                 to: path.join(__dirname, 'www', 'lib', 'react.js')
-            }, 
-            { 
-                from: path.join(__dirname, 'node_modules', 'react-dom', 'dist', 'react-dom.min.js'), 
+            },
+            {
+                from: path.join(__dirname, 'node_modules', 'react-dom', 'dist', 'react-dom.min.js'),
                 to: path.join(__dirname, 'www', 'lib', 'react-dom.js')
             },
-            { 
-                from: path.join(__dirname, 'node_modules', 'react-bootstrap', 'dist', 'react-bootstrap.min.js'), 
+            {
+                from: path.join(__dirname, 'node_modules', 'react-bootstrap', 'dist', 'react-bootstrap.min.js'),
                 to: path.join(__dirname, 'www', 'lib', 'react-bootstrap.js')
             }
         ])
